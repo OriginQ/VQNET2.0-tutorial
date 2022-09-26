55fe35da44d73215d67c44597d81344c2387ac9d
王汉超 王汉超 <whc@originqc.com>
2022-09-26 11:21:15 +0800

    [ref] conf.py version 2.0.4

--- source/conf.py
+++ source/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Original Quantum'
 author = 'Original Quantum'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2.0.3'
+release = 'v2.0.4'
 
 
 # -- General configuration ---------------------------------------------------
